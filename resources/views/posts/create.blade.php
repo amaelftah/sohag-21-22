96eff2ad038f9230a559e8b7caa9b7e84fdd6121
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2022-01-29 13:23:22 +0200

    Add template inheritance

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,1 +1,28 @@
-we are in create
+@extends('layouts.app')
+
+@section('title') Create @endsection
+
+@section('content')
+        <form>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Title</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1">
+            </div>
+            
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>  
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Post Creator</label>
+                <select class="form-control">
+                    <option value="1">ahmed</option>
+                    <option value="2">mohamed</option>
+                </select>
+            </div>
+            
+            <button class="btn btn-success">Create Post</button>
+        </form>
+@endsection
+    
